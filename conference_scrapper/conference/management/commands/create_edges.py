af8d09fa34d1565e14cf1273c8bd868d387c49bc
Magomed <m.aliev@alabom.com>
2019-03-17 22:53:18 +0300

    Added command, modified dockerfile

---
+++ conference_scrapper/conference/management/commands/create_edges.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import json
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Parses files with raw data and create csv file with edges.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('filenames', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        filenames, separator = options['filenames'], '#'
+        for name in filenames:
+            with open(name) as f:
+                data = json.load(f)
+            edge_list = []
+            for i in range(len(data)):
+                for j in range(i + 1, len(data)):
+                    matches = list(set(data[i]['categories']).intersection(data[j]['categories']))
+                    if (separator in str(matches)):
+                        print("Separator '" + separator + "' is used in categories: " + str(matches))
+                        return -1
+                    if (len(matches) != 0):
+                        edge = {'id_1': data[i]['id'],
+                                'id_2': data[j]['id'],
+                                'n': str(len(matches)),
+                                'matches': separator.join(map(lambda x: x.strip(), matches))
+                                }
+                        edge_list.append(edge)
+            df = pd.DataFrame(edge_list, columns=['id_1', 'id_2', 'n', 'matches'])
+            df.to_csv(f'data/{name[:-5]}.csv', index=False)
