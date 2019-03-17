8eb1f86f2c5410693fc8e2e8f99b18f295ccdf64
Magomed <m.aliev@alabom.com>
2019-03-17 21:24:53 +0300

    Cleanup

--- conference_scrapper/conference/models.py
+++ conference_scrapper/conference/models.py
@@ -5,8 +5,8 @@ from django.contrib.postgres import fields
 class Conference(models.Model):
     title = models.CharField(max_length=255)
     year = models.IntegerField(null=True, blank=True)
-    key_dates = fields.JSONField(default=dict)
+    key_dates = fields.JSONField(default=dict, null=True, blank=True)
     start_date = models.DateTimeField(null=True, blank=True)
     end_date = models.DateTimeField(null=True, blank=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(null=True, blank=True)
     key_words = fields.ArrayField(models.CharField(max_length=64), null=True, blank=True)
