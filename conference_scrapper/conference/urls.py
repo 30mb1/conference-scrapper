e20d5a7dd146d2ac2c3bb42e3201dd7811404972
Magomed <m.aliev@alabom.com>
2019-03-25 01:39:45 +0300

    Added download button (back only)

--- conference_scrapper/conference/urls.py
+++ conference_scrapper/conference/urls.py
@@ -3,5 +3,6 @@ from conference_scrapper.conference import views
 
 urlpatterns = [
     path('graph/<str:source>', views.GraphView.as_view()),
+    path('download/', views.DownloadView.as_view()),
     path('', views.SearchView.as_view(), name="home")
 ]
