2edf45a40763298672e2f000dbc65b821157368b
Magomed <m.aliev@alabom.com>
2019-03-26 13:11:14 +0300

    Fix

--- conference_scrapper/conference/urls.py
+++ conference_scrapper/conference/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from conference_scrapper.conference import views
 
 urlpatterns = [
-    path('graph/', views.GraphView.as_view()),
     path('graph/v2', views.NickGrpah.as_view()),
+    path('graph/<str:source>', views.GraphView.as_view()),
     path('game/<int:vertex>', views.GameView.as_view()),
     path('download/', views.DownloadView.as_view()),
     path('', views.SearchView.as_view(), name="home")
